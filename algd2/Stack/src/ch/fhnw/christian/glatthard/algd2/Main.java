07c86b6c4f1d8dbaccee0dd034bf7565be984f5e
Chregi Glatthard <chregi.glatthard@gmail.com>
2012-10-11 07:34:13 +0200

    Sackbahnhof Beschreibung

--- algd2/Stack/src/ch/fhnw/christian/glatthard/algd2/Main.java
+++ algd2/Stack/src/ch/fhnw/christian/glatthard/algd2/Main.java
@@ -1,5 +1,11 @@
 package ch.fhnw.christian.glatthard.algd2;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+
 public class Main {
 
 	/**
@@ -20,6 +26,38 @@ public class Main {
 		System.out.println(stack.pop());
 		System.out.println(stack.pop());
 		System.out.println(stack.isEmpty());
+		
+		// Sackbahnhof
+		System.out.println("-----------------------------------------------");
+		System.out.println("");
+		System.out.println("Das folgende Programm sortiert die Züge auf S1 und S2 mithilfe von S3.");
+		System.out.println("Alle A kommen auf S1.");
+		System.out.println("Alle A kommen auf S2.");
+		System.out.println("");
+		System.out.println("      _________________________________    S1");
+		System.out.println("     /");
+		System.out.println("____/__________________________________    S2");
+		System.out.println("    \\");
+		System.out.println("     \\_________________________________    S3");
+		System.out.println("");
+		
+		
+		try{
+		    BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+		    System.out.println("Geben Sie eine beliebige Abfolge von A und B ein für S1. (max 8)");
+		    String s1 = bufferRead.readLine();
+		    System.out.println(s1);
+		    
+		    System.out.println("Geben Sie eine beliebige Abfolge von A und B ein für S2. (max 8)");
+		    String s2 = bufferRead.readLine();
+		    System.out.println(s2);
+		    
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
 	}
 
 }
