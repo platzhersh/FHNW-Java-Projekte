01ac380cdd86e034e838f6b2c96dc5c338df06ae
Chregi Glatthard <chregi.glatthard@gmail.com>
2013-02-21 14:17:34 +0100

    vesys & conpr created

---
+++ vesys/bank/src/bank/gui/tests/EfficiencyTest.java
@@ -0,0 +1,48 @@
+package bank.gui.tests;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import bank.Account;
+import bank.Bank;
+
+public class EfficiencyTest implements BankTest {
+
+	final static int NUMBER_OF_EFF_TESTS = 1000; // TODO to be defined over
+													// construcotr
+
+	@Override
+	public String getName() {
+		return "Efficiency Test";
+	}
+
+	@Override
+	public boolean isEnabled(int size) {
+		return size > 0;
+	}
+
+	@Override
+	public void runTests(JFrame context, Bank bank, String currentAccountNumber) throws Exception {
+		final Account acc = bank.getAccount(currentAccountNumber);
+
+		String msg;
+		try {
+			System.gc();
+			long st = System.currentTimeMillis();
+			for (int i = 1; i <= NUMBER_OF_EFF_TESTS; i++) {
+				acc.deposit(i);
+				acc.withdraw(i);
+			}
+			st = System.currentTimeMillis() - st;
+			msg = 2 * NUMBER_OF_EFF_TESTS + " operations in " + st / 1000.0
+					+ " Sek\n" + st / (2.0 * NUMBER_OF_EFF_TESTS) + " msec/op";
+		} catch (Exception e) {
+			msg = "test did throw an exception\n" + e.getMessage();
+		}
+
+		JOptionPane.showMessageDialog(context, msg, "Test Result",
+				JOptionPane.INFORMATION_MESSAGE);
+
+	}
+
+}
---
+++ vesys/bank/src/bank/local/Driver.java
@@ -0,0 +1,116 @@
+package bank.local;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import bank.InactiveException;
+import bank.OverdrawException;
+
+public class Driver implements bank.BankDriver {
+	private Bank bank = null;
+
+	@Override
+	public void connect(String[] args) {
+		bank = new Bank();
+		System.out.println("connected...");
+	}
+
+	@Override
+	public void disconnect() {
+		bank = null;
+		System.out.println("disconnected...");
+	}
+
+	@Override
+	public Bank getBank() {
+		return bank;
+	}
+
+	static class Bank implements bank.Bank {
+
+		private Map<String, Account> accounts = new HashMap<String, Account>();
+
+		@Override
+		public Set<String> getAccountNumbers() {
+
+			// TODO remove
+			Account acc = new Account("dini mueter");
+			accounts.put("123", acc);
+			accounts.entrySet();
+			
+			return accounts.keySet(); // FIXME has to be replaced
+		}
+
+		@Override
+		public String createAccount(String owner) {
+			System.out.println("Bank.createAccount has to be implemented");
+			return null;
+		}
+
+		@Override
+		public boolean closeAccount(String number) {
+			System.out.println("Bank.closeAccount has to be implemented");
+			return false;
+		}
+
+		@Override
+		public bank.Account getAccount(String number) {
+			return (bank.Account) accounts.get(number);
+		}
+
+		@Override
+		public void transfer(bank.Account from, bank.Account to, double amount)
+				throws IOException, InactiveException, OverdrawException {
+			System.out.println("Bank.transfer has to be implemented");
+		}
+
+	}
+
+	static class Account implements bank.Account {
+		private String number;
+		private String owner;
+		private double balance;
+		private boolean active = true;
+
+		Account(String owner) {
+			this.owner = owner;
+			// FIXME number has to be set
+		}
+
+		@Override
+		public double getBalance() {
+			return balance;
+		}
+
+		@Override
+		public String getOwner() {
+			return owner;
+		}
+
+		@Override
+		public String getNumber() {
+			return number;
+		}
+
+		@Override
+		public boolean isActive() {
+			return active;
+		}
+
+		@Override
+		public void deposit(double amount) throws InactiveException {
+			System.out.println("Account.deposit has to be implemented");
+		}
+
+		@Override
+		public void withdraw(double amount) throws InactiveException, OverdrawException {
+			System.out.println("Account.withdraw has to be implemented");
+		}
+
+	}
+
+}
