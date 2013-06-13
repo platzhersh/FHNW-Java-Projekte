96c62553b149de7eb758bad4371f42f249d070d6
Chregi Glatthard <chregi.glatthard@gmail.com>
2013-06-13 12:50:46 +0200

    algd1, Backtracking: Optimierung und Kommentare

--- algd1/08_Backtracking/src/ch/fhnw/algd1/Skyscrapers2.java
+++ algd1/08_Backtracking/src/ch/fhnw/algd1/Skyscrapers2.java
@@ -2,8 +2,9 @@ package ch.fhnw.algd1;
 
 public class Skyscrapers2 {
 	
-	int n;
-	int[][] map;
+	int n;			// Grösse der n*n Matrix
+	int[][] map;	// (n+2)*(n+2) Matrix - Skyscrapers & Zähler
+	int stepCount;
 	
 	/**
 	 * @param args
@@ -12,11 +13,15 @@ public class Skyscrapers2 {
 		Skyscrapers2 sky = new Skyscrapers2();
 		System.out.println(sky.toString());
 		sky.solve();
+		System.out.println("\nNumber of solveField() calls: "+sky.getStepCount());
 	}
-	
+	/***
+	 * Initialisiert ein 6*6 Array und schreibt die vorgegebenen Zähler in die erste und letzte Zeile / Spalte 
+	 */
 	public Skyscrapers2() {
 		n = 6;
 		map = new int[n][n];
+		stepCount = 0;
 
 		/* Counters Top */
 		map[0][1] = 1;
@@ -43,9 +48,14 @@ public class Skyscrapers2 {
 		map[5][4] = 3;
 		
 	}
-	
+
+	public int getStepCount() {
+		return this.stepCount;
+	}
+	/***
+	 * Überall wo nur 1 Skyscraper gesehen wird, muss gleich anliegend ein Skyscraper der maximalen Höhe stehen
+	 */
 	public void setOnes() {
-		// Überall wo nur 1 Skyscraper gesehen wird, muss gleich anliegend ein Skyscraper der maximalen Höhe stehen
 		int x = 0; int y = 0;
 
 		// Left Column
@@ -77,6 +87,11 @@ public class Skyscrapers2 {
 		System.out.println(this.toString());
 	}
 
+	
+	/**
+	 * Backtracking Hauptmethode
+	 * Löst die Matrix Feld umd Feld, Zeile um Zeile
+	 */
 	public void solve () {
 		
 		this.setOnes();
@@ -85,11 +100,19 @@ public class Skyscrapers2 {
 			solveRowField(1,posY);
 		}
 	}
+
+	/**
+	 * Wird jeweils rekursiv für jede Spalte aufgerufen um die Zeile y zu lösen
+	 * Im Fehlerfalle springt der Compiler zurück in die Oberfunktion (vorheriges Feld)
+	 * @param x Spaltennummer des aktuellen Feldes
+	 * @param y Zeilennummer des aktuellen Feldes
+	 * @return gibt TRUE zurück wenn die Zeile korrekt ist
+	 */
 	public boolean solveRowField(int x, int y) {
-		int t = n-1;
+		int t = n;
 		boolean nextField = false;
 		while (!nextField) {
-			if (solveField(x,y,t) == false) return false;
+			if (solveField(x,y,--t) == false) return false;
 			else {
 				boolean topdown = true;
 				if (y == n-2) {
@@ -101,12 +124,21 @@ public class Skyscrapers2 {
 						map[x][y] = 0;
 						return false; 
 					} else return topdown;
-				} else { nextField = solveRowField(x+1,y); t--; }
+				} else { t--; nextField = solveRowField(x+1,y); }
 			}
 		}
 		return true;
 	}
+	
+	/**
+	 * 
+	 * @param x Spaltennummer des aktuellen Feldes
+	 * @param y Zeilennummer des aktuellen Feldes
+	 * @param t maximaler Wert den es zu testen gibt, solveField probiert für alle Werte < t bis ein gültiger gefunden wird
+	 * @return gibt TRUE zurück wenn ein gültiger Wert gefunden wurde, FALSE wenn kein gültiger Wert gefunden werden konnte (t=0)
+	 */
 	public boolean solveField(int x, int y, int t) {
+		this.stepCount++;
 		while (t > 0) {
 			t--; map[x][y] = t;
 			System.out.println("Row: "+y+", Col: "+x+", t="+t);
@@ -117,10 +149,12 @@ public class Skyscrapers2 {
 
 	}
 	
-	public boolean isValid(int x, int y){
-		return validateRow(x,y) && validateCol(x,y) && countLeft(y) && countRight(y) && countTop(x) && countBottom(x);
-	}
-	
+	/**
+	 * Checkt ob der Feldwert in der Zeile eindeutig ist
+	 * @param x Spaltennummer des zu prüfenden Feldes
+	 * @param y Zeilennumer des zu prüfenden Feldes
+	 * @return
+	 */
 	public boolean validateRow(int x, int y) {
 		int validateMe = map[x][y];
 		int xEnd = n-2;
@@ -164,6 +198,12 @@ public class Skyscrapers2 {
 		return count == ref;
 	}
 	
+	/**
+	 * Checkt ob der Feldwert in der Spalte eindeutig ist
+	 * @param x Spaltennummer des zu prüfenden Feldes
+	 * @param y Zeilennumer des zu prüfenden Feldes
+	 * @return
+	 */
 	public boolean validateCol(int x, int y) {
 		int validateMe = map[x][y];
 		int yEnd = n-2;
@@ -206,7 +246,6 @@ public class Skyscrapers2 {
 		}
 		return count == ref;
 	}
-	
 	
 	
 	@Override
