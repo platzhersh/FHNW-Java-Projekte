2b2001f1e7b4eed1ea9eb15ac93847c5c448eeaf
Chregi Glatthard <chregi.glatthard@gmail.com>
2013-06-26 16:11:14 +0200

    kpsp - Lösungen Zwischenprüfung FS2009

--- kpsp/KPSP_Zwischenprüfung_FS2009/kpsp_FS2009.hs
+++ kpsp/KPSP_Zwischenprüfung_FS2009/kpsp_FS2009.hs
@@ -101,7 +101,132 @@ minList3 xs = foldleft1 minCur xs
 
 {-
 	Aufgabe 1.7
-
 	
+	Minimum und Maximum von möglicherweise leeren Listen liessen sich leicht definieren, wenn wir die uns zur Verfügung stehenden ganzen Zahlen Int ergänzen würden durch die zwei weiteren "Werte" -Inf und +Inf. In Haskell können wir das leicht durch folgenden neuen Typ IntIf realisieren:
 
 -}
+
+data IntInf
+  = MinusInf
+  | Normal Int
+  | PlusInf
+  
+instance Show IntInf where
+  show (Normal x) = show x
+  show MinusInf = "- Infinity"
+  show PlusInf = "+ Infinity"
+  
+{- 
+	Dann könnten wir die Ordnungsrelation als Funktion less zwischen zwei Werten vom Typ IntIf definieren durch folgende Tabelle:
+	
+	less	-Inf	m: Int	+Inf
+	--------------------------------
+	-Inf	false	true	true
+	n: Int	false	n < m	true
+	+Inf	false	false	false
+-}
+
+less :: IntInf -> IntInf -> Bool
+less MinusInf MinusInf = False
+less MinusInf _ = True
+
+less (Normal x) MinusInf = False
+less (Normal x) (Normal y) = x < y
+less (Normal x) PlusInf = True
+
+less PlusInf _ = False
+
+{-
+	Schliesslich können wir eine Funktion minListInf definieren, die das kleinste Element einer Liste von Werten vom Typ IntInf bestimmt - selbst wenn die Liste leer ist.
+	
+	e) Schreiben Sie die Funktion minListInf. Hinweis: mit Vorteil schreiben Sie dazu zuerst eine Funktion minInf, die den kleineren von zwei Werten vom Typ IntInf bestimmt.
+-}
+
+minInf :: IntInf -> IntInf -> IntInf
+minInf x y
+  | less x y = x
+  | otherwise = y
+
+minListInf :: [IntInf] -> IntInf
+minListInf [] = PlusInf
+minListInf (x:xs) = minInf x (minListInf xs)
+  
+minListInf2 :: [IntInf] -> IntInf
+minListInf2 [] = PlusInf
+minListInf2 (x:xs) 
+  | less x y = x
+  | otherwise = y
+  where y = minListInf2 xs
+  
+{-
+	Aufgabe 3
+	
+	Betrachten Sie eine Funktion delDups, die eine Liste von Elementen als Eingabe nimmt und eine Liste von Elementen als Ausgabe liefert. Die Ausgabeliste enthält dieselben Elemente wie die Eingabeliste, jedoch jedes Element höchstens einmal. Beispiel:
+	
+	delDups [3,5,3,4,7,4,4,3,6] = [3,5,4,7,6]
+
+-}
+
+delDups :: Eq a => [a] -> [a]
+delDups [] = []
+delDups (x:xs) = [x] ++ delDups (filter (\y -> y/=x) xs)
+
+{-
+	Aufgabe 4.1
+	
+	Boolesche Ausdrücke sind entweder:
+	- eine boolesche Konstante (true, T oder false, F), oder
+	- die Negation (not) eines booleschen Ausdrucks, oder
+	- die Konjunktion (and) von zwei booleschen Ausdrücken, oder
+	- die Disjunktion (or) von zwei booleschen Ausdrücken.
+	
+	Beispiel für einen booleschen Ausdruck:
+	
+	(T and F) or not F
+	
+	a) Schreiben Sie einen neuen Datentyp BExpr zur Darstellung boolescher Ausdrücke
+	b) Stellen Sie unseren Beispielausdruck mit diesem Datentyp dar.
+-}
+
+-- a)
+
+data BExpr = Konstante Bool
+  | Negation BExpr
+  | Konjunktion BExpr BExpr
+  | Disjunktion BExpr BExpr
+  
+instance Show BExpr where
+  show (Konstante b) = show b
+  show (Negation b) = "(not " ++ show b ++ ")"
+  show (Konjunktion a b) = "(" ++ show a ++ " and " ++ show b ++ ")"
+  show (Disjunktion a b) = "(" ++ show a ++ " or " ++ show b ++ ")"
+
+-- b)
+example = Disjunktion (Konjunktion (Konstante True) (Konstante False)) (Negation (Konstante False))
+
+{-
+	Aufgabe 4.2
+	
+	Betrachten Sie eine Funktion eval, die einen booleschen Ausdruck nach den bekannten Regeln für not, and und or auswertet. Die Auswertung unseres Beispielausdrucks liefert den Wert true.
+
+-}
+
+eval :: BExpr -> Bool
+eval (Konstante b) = b
+eval (Konjunktion a b) = (eval a) && (eval b)
+eval (Disjunktion a b) = (eval a) || (eval b)
+eval (Negation b) = not (eval b)
+
+{-
+	Aufgabe 4.3 
+	
+	Zwei boolesche Ausdrücke A und B lassen sich auch durch eine Implikation (implies, ->) verknüpfen. Unser Datetyp hat zwar keinen Konstruktor für die Implifikation, aber die Implikation lässt sich gemäss folgendem Zusammenhang leicht über die Negation und Disjunktion darstellen:
+	
+	A -> B = ¬ A or B
+	
+	Betrachten Sie eine Funktion makeImpl, die zwei boolesche Ausdrücke A und B als Eingabe nimmt und einen booleschen Ausdruck a -> B als Ausgabe liefert, entsprechend dem eben genannten Zusammenhang.
+
+-}
+
+makeImpl :: BExpr -> BExpr -> BExpr
+makeImpl a b = Disjunktion (Negation a) b
