f7931302a5f7cbc8d04a6ad263899a734bb1a869
Chregi Glatthard <chregi.glatthard@gmail.com>
2013-09-20 14:20:59 +0200

    prcpp - First Commit

---
+++ prcpp/First/First/main.cpp
@@ -0,0 +1,10 @@
+#include "stdio.h"
+#include <iostream>
+
+
+int main(int argc, char* argv[]) {
+	printf("The program arguments are: \n");
+	for (int i = 0; i < argc; i++)
+		printf("%d: %s\n", i, argv[i]);
+	std::cin;
+}
---
+++ prcpp/First/First/main.h
@@ -0,0 +1,8 @@
+#pragma once
+class main
+{
+public:
+	main(void);
+	~main(void);
+};
+
